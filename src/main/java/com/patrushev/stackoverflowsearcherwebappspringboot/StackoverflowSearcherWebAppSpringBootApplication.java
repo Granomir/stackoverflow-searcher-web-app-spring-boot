81d493ca791723809fb4fa6c6101e5e1abf79d1f
Granomir <banan4eg1989@gmail.com>
2018-11-27 11:13:42 +0300

    пустой проект. Сбрасывать до этого состояния

---
+++ src/main/java/com/patrushev/stackoverflowsearcherwebappspringboot/StackoverflowSearcherWebAppSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.patrushev.stackoverflowsearcherwebappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowSearcherWebAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowSearcherWebAppSpringBootApplication.class, args);
+	}
+}
