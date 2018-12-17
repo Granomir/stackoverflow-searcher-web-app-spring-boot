5c9ebbc590861685e7e0e96e3befcaec1831bd25
Granomir <banan4eg1989@gmail.com>
2018-12-17 13:46:15 +0300

    1

--- src/main/java/com/patrushev/stackoverflowsearcherwebappspringboot/controller/MainController.java
+++ src/main/java/com/patrushev/stackoverflowsearcherwebappspringboot/controller/MainController.java
@@ -15,26 +15,20 @@ import java.util.List;
 @Controller
 public class MainController {
 
-    //в ответ на переход по localhost выдается страница index:
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
     public String showIndex(Model model) {
-        //видимо создается объект, свойства которого будут заполняться на странице из формы запроса
         QueryForm queryForm = new QueryForm();
         model.addAttribute("queryForm", queryForm);
 
         return "index";
     }
 
-    //в ответ на запрос по /index с методов POST выполняется обновление index:
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.POST)
     public String doQuery(Model model, @ModelAttribute("queryForm") QueryForm queryForm) throws IOException {
         List<QuestionResult> questions;
         String query = queryForm.getQuery();
-        //проверка на пустой запрос
         if (query != null && query.length() > 0) {
-            //запрос в API и создание коллекции из ответов
             questions = StackExchangeService.getInstance().getQuestionsList(query);
-            //проверка пустой коллекции ответов
             if (questions == null || questions.isEmpty()) {
                 model.addAttribute("errorNoResult", "Nothing found by the request \"" + query + "\"");
                 return "index";
